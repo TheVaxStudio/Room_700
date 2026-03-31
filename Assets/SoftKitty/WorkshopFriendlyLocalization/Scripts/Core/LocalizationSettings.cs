6e8882f11b699d210f9f5bc1ef7846697c3cc923
TheVaxStudio <rodrigomassanori2014@gmail.com>
2026-03-31 13:52:34 -0300

    all

--- Assets/SoftKitty/WorkshopFriendlyLocalization/Scripts/Core/LocalizationSettings.cs
+++ Assets/SoftKitty/WorkshopFriendlyLocalization/Scripts/Core/LocalizationSettings.cs
@@ -7,25 +7,32 @@ namespace SoftKitty.WSFL
     public class LocalizationSettings : ScriptableObject
     {
         #region Variables
-        public string DefaultLanguageName = "English";
+        public string DefaultLanguageName = "Portuguese";
+
         public string LocalizationRelativePath = "";
+
         public string BuildLocalizationRelativePath = "/LocalizationFiles";
+        
         public List<string> LocalizationTextList = new List<string>();
 
         public const string CONFIG_NAME = "com.SoftKitty.Localization.settings";
 
-        private static LocalizationSettings instance;
+        static LocalizationSettings instance;
         #endregion
 
         #region Methods
-        private void OnEnable()
+        void OnEnable()
         {
             if (instance == null) instance = this;
         }
 
         public static string GetLocalizationPath()
         {
-            if (instance == null) Load();
+            if (instance == null)
+            {
+                Load();
+            }
+            
 #if UNITY_EDITOR
             return Application.dataPath + instance.LocalizationRelativePath;
 #else
@@ -35,7 +42,10 @@ namespace SoftKitty.WSFL
 
         public static LocalizationSettings Load()
         {
-            if (instance) return instance;
+            if (instance)
+            {
+                return instance;
+            }
 #if UNITY_EDITOR
             UnityEditor.EditorBuildSettings.TryGetConfigObject(CONFIG_NAME, out instance);
 #else
