242e472bb83ff0ad0bb10386d39efa69deb0741f
TheVaxStudio <rodrigomassanori2014@gmail.com>
2026-04-01 11:42:52 -0300

    saved

--- Assets/Scripts/ProceduralTilemap/ProceduralTilemapGenerator.cs
+++ Assets/Scripts/ProceduralTilemap/ProceduralTilemapGenerator.cs
@@ -1,39 +1,51 @@
 using System.Collections.Generic;
+using Random = System.Random;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityRandom = UnityEngine.Random;
 
 public class ProceduralTilemapGenerator : MonoBehaviour
 {
     [Header("Tilemap Settings")]
-    public Tilemap Tilemap;
+    public Tilemap MapTile;
+
     public TileBase FloorTile;
+
     public TileBase WallTile;
 
     [Header("Generation Settings")]
     public int Width = 50;
+
     public int Height = 50;
+
     public int MinRoomSize = 3;
+
     public int MaxRoomSize = 10;
+
     public int MaxRooms = 10;
+
     public bool UseRandomSeed = true;
+
     public int Seed = 0;
 
-    private System.Random Random;
+    Random Rdn;
 
     void Start()
     {
         if (UseRandomSeed)
         {
-            Seed = UnityEngine.Random.Range(0, int.MaxValue);
+            Seed = UnityRandom.Range(0, int.MaxValue);
         }
-        Random = new System.Random(Seed);
+
+        Rdn = new Random(Seed);
+        
         GenerateDungeon();
     }
 
     void GenerateDungeon()
     {
         // Clear the tilemap
-        Tilemap.ClearAllTiles();
+        MapTile.ClearAllTiles();
 
         // Create a 2D array to represent the map
         int[,] Map = new int[Width, Height];
@@ -49,21 +61,27 @@ public class ProceduralTilemapGenerator : MonoBehaviour
 
         // Generate rooms
         List<RectInt> Rooms = new List<RectInt>();
+
         for (int i = 0; i < MaxRooms; i++)
         {
-            int roomWidth = Random.Next(MinRoomSize, MaxRoomSize + 1);
-            int roomHeight = Random.Next(MinRoomSize, MaxRoomSize + 1);
-            int roomX = Random.Next(1, Width - roomWidth - 1);
-            int roomY = Random.Next(1, Height - roomHeight - 1);
+            int RoomWidth = Rdn.Next(MinRoomSize, MaxRoomSize + 1);
 
-            RectInt NewRoom = new RectInt(roomX, roomY, roomWidth, roomHeight);
+            int RoomHeight = Rdn.Next(MinRoomSize, MaxRoomSize + 1);
+
+            int RoomX = Rdn.Next(1, Width - RoomWidth - 1);
+            
+            int RoomY = Rdn.Next(1, Height - RoomHeight - 1);
+
+            RectInt NewRoom = new RectInt(RoomX, RoomY, RoomWidth, RoomHeight);
 
             bool Overlaps = false;
-            foreach (RectInt room in Rooms)
+
+            foreach (RectInt Room in Rooms)
             {
-                if (NewRoom.Overlaps(room))
+                if (NewRoom.Overlaps(Room))
                 {
                     Overlaps = true;
+
                     break;
                 }
             }
@@ -73,9 +91,9 @@ public class ProceduralTilemapGenerator : MonoBehaviour
                 Rooms.Add(NewRoom);
 
                 // Carve out the room
-                for (int x = roomX; x < roomX + roomWidth; x++)
+                for (int x = RoomX; x < RoomX + RoomWidth; x++)
                 {
-                    for (int y = roomY; y < roomY + roomHeight; y++)
+                    for (int y = RoomY; y < RoomY + RoomHeight; y++)
                     {
                         Map[x, y] = 0; // 0 = floor
                     }
@@ -86,19 +104,25 @@ public class ProceduralTilemapGenerator : MonoBehaviour
         // Connect rooms with corridors
         for (int i = 1; i < Rooms.Count; i++)
         {
-            Vector2Int PrevCenter = new Vector2Int(Rooms[i - 1].x + Rooms[i - 1].width / 2, Rooms[i - 1].y + Rooms[i - 1].height / 2);
-            Vector2Int CurrCenter = new Vector2Int(Rooms[i].x + Rooms[i].width / 2, Rooms[i].y + Rooms[i].height / 2);
+            Vector2Int PrevCenter = new Vector2Int(Rooms[i - 1].x + Rooms[i - 1].width / 2,
+            Rooms[i - 1].y + Rooms[i - 1].height / 2);
+            
+            Vector2Int CurrCenter = new Vector2Int(Rooms[i].x + Rooms[i].width / 2,
+            Rooms[i].y + Rooms[i].height / 2);
 
-            if (Random.Next(0, 2) == 0)
+            if (Rdn.Next(0, 2) == 0)
             {
                 // Horizontal then vertical
                 CreateHorizontalCorridor(Map, PrevCenter.x, CurrCenter.x, PrevCenter.y);
+
                 CreateVerticalCorridor(Map, PrevCenter.y, CurrCenter.y, CurrCenter.x);
             }
+
             else
             {
                 // Vertical then horizontal
                 CreateVerticalCorridor(Map, PrevCenter.y, CurrCenter.y, PrevCenter.x);
+
                 CreateHorizontalCorridor(Map, PrevCenter.x, CurrCenter.x, CurrCenter.y);
             }
         }
@@ -109,13 +133,15 @@ public class ProceduralTilemapGenerator : MonoBehaviour
             for (int y = 0; y < Height; y++)
             {
                 Vector3Int Position = new Vector3Int(x, y, 0);
+
                 if (Map[x, y] == 0)
                 {
-                    Tilemap.SetTile(Position, FloorTile);
+                    MapTile.SetTile(Position, FloorTile);
                 }
+
                 else
                 {
-                    Tilemap.SetTile(Position, WallTile);
+                    MapTile.SetTile(Position, WallTile);
                 }
             }
         }
@@ -124,7 +150,9 @@ public class ProceduralTilemapGenerator : MonoBehaviour
     void CreateHorizontalCorridor(int[,] Map, int XStart, int XEnd, int Y)
     {
         int Start = Mathf.Min(XStart, XEnd);
+
         int End = Mathf.Max(XStart, XEnd);
+
         for (int x = Start; x <= End; x++)
         {
             Map[x, Y] = 0;
@@ -134,7 +162,9 @@ public class ProceduralTilemapGenerator : MonoBehaviour
     void CreateVerticalCorridor(int[,] Map, int YStart, int YEnd, int X)
     {
         int Start = Mathf.Min(YStart, YEnd);
+
         int End = Mathf.Max(YStart, YEnd);
+
         for (int y = Start; y <= End; y++)
         {
             Map[X, y] = 0;
